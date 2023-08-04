6d83fd10d30c093eb53480495067b219bc63341f
Akash0000001 <akashranjan947@gmail.com>
2023-08-04 22:21:24 +0530

    Added login backend api

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const Users=require("../models/user")
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 exports.addUser=(req,res,next)=>{
     const {name,email,password}=req.body
     const saltrounds=10;
@@ -26,4 +27,43 @@ catch(err)
     res.status(400).send(err)
 }
 })
+}
+
+function generateaccesstoken(id,name)
+{
+    return jwt.sign({userId:id,name:name},"SGHP07899yuiichjkqicnms6789")
+}
+exports.authorizeUser=async (req,res,next)=>{
+    try{
+    const users=await Users.findAll()
+        for(let i=0;i<users.length;i++)
+        {
+            if(users[i].email===req.body.email)
+            {
+                return bcrypt.compare(req.body.password,users[i].password,(err,result)=>{
+                    if(err)
+                    {
+                        throw new Error("Something went wrong")
+                    }
+                    if(result===true)
+                    {
+                        res.status(200).json({success:true,message:"User logged in successfully",token:generateaccesstoken(users[i].id,users[i].name)})
+                    }
+                    else
+                    {
+                        res.status(401).json({success:false,message:"User not authorized"})
+                    }
+
+                })
+                 
+            }
+            
+        }
+    
+        return res.status(404).json({success:false,message:"User not found"})
+    }
+    catch(err)
+    {
+    res.status(500).json({message:err,success:false})
+    }
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const userController=require("../controllers/user")
 
 router.post("/signup",userController.addUser)
+router.post("/login",userController.authorizeUser)
 
     
 module.exports=router
